8e4c2c13f120061459a8ae77e8d41839bc9fa050
Xgsjb <2143696319@qq.com>
2024-10-02 12:54:07 +0800

    Update AndroidAudio.h

--- src/android/app/src/main/cpp/AndroidAudio.h
+++ src/android/app/src/main/cpp/AndroidAudio.h
@@ -4,6 +4,14 @@
 
 namespace AndroidAudio
 {
-	void createAudioDevice(IAudioAPI::AudioAPI audioApi, sint32 channels, sint32 volume, bool isTV = true);
-	void setAudioVolume(sint32 volume, bool isTV = true);
+    // 添加 LC3 枚举值
+    enum class AudioAPI
+    {
+        Default,
+        Cubeb,
+        LC3 // 新增的 LC3 音频编码格式
+    };
+
+    void createAudioDevice(AudioAPI audioApi, sint32 channels, sint32 volume, bool isTV = true);
+    void setAudioVolume(sint32 volume, bool isTV = true);
 }; // namespace AndroidAudio
